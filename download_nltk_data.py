6e1dcb60b5770f58e4a447d4042fd8970340b76f
Godfather3500 <harpreerkm@gmail.com>
2025-09-12 20:48:43 +0530

    main

--- download_nltk_data.py
+++ download_nltk_data.py
@@ -1,10 +1,9 @@
 import nltk
-
 try:
-    # Attempt to download the 'stopwords' corpus
-    nltk.download('stopwords', download_dir='/opt/render/project/src/.nltk_data', quiet=True)
-    # Attempt to download the 'punkt' corpus, which is also needed for tokenization
-    nltk.download('punkt', download_dir='/opt/render/project/src/.nltk_data', quiet=True)
-    print("NLTK corpora downloaded successfully.")
+        # Download 'stopwords' and 'punkt' to a accessible location
+        nltk.download('stopwords', download_dir='/opt/render/project/src/.nltk_data', quiet=True)
+        nltk.download('punkt', download_dir='/opt/render/project/src/.nltk_data', quiet=True)
+        print("NLTK corpora downloaded successfully.")
 except Exception as e:
-    print(f"Error downloading NLTK corpora: {e}")
+        print(f"Error downloading NLTK corpora: {e}")
+    
