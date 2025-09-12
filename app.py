3f640e5f02fce414f0d40ed3ddee0f88a8be6bb5
Godfather3500 <harpreerkm@gmail.com>
2025-09-12 21:23:41 +0530

    main

--- app.py
+++ app.py
@@ -7,19 +7,44 @@ import seaborn as sb
 import re
 import nltk
 import pickle
+import ssl
 from sklearn.feature_extraction.text import TfidfVectorizer
 from matplotlib import pyplot as plt
 from sklearn.linear_model import PassiveAggressiveClassifier
 from nltk.stem import WordNetLemmatizer
 from nltk.corpus import stopwords
 
-app = Flask(__name__,template_folder='./templates',static_folder='./static')
+# Handle SSL certificate issues for NLTK downloads
+try:
+    _create_unverified_https_context = ssl._create_unverified_context
+except AttributeError:
+    pass
+else:
+    ssl._create_default_https_context = _create_unverified_https_context
 
+# Download required NLTK data
+def download_nltk_data():
+    try:
+        nltk.data.find('corpora/stopwords')
+        nltk.data.find('tokenizers/punkt')
+        nltk.data.find('corpora/wordnet')
+    except LookupError:
+        print("Downloading required NLTK data...")
+        nltk.download('stopwords', quiet=True)
+        nltk.download('punkt', quiet=True)
+        nltk.download('wordnet', quiet=True)
+        print("NLTK data downloaded successfully")
+
+# Download NLTK data before initializing the app
+download_nltk_data()
+
+app = Flask(__name__, template_folder='./templates', static_folder='./static')
+
+# Load models and initialize NLTK components
 loaded_model = pickle.load(open("model.pkl", 'rb'))
 vector = pickle.load(open("vector.pkl", 'rb'))
 lemmatizer = WordNetLemmatizer()
 stpwrds = set(stopwords.words('english'))
-corpus = []
 
 def fake_news_det(news):
     review = news
@@ -27,43 +52,40 @@ def fake_news_det(news):
     review = review.lower()
     review = nltk.word_tokenize(review)
     corpus = []
-    for y in review :
-        if y not in stpwrds :
+    for y in review:
+        if y not in stpwrds:
             corpus.append(lemmatizer.lemmatize(y))
     input_data = [' '.join(corpus)]
     vectorized_input_data = vector.transform(input_data)
     prediction = loaded_model.predict(vectorized_input_data)
-     
     return prediction
-
-        
 
 @app.route('/')
 def home():
     return render_template('index.html')
 
-@app.route('/about',methods=['GET'])
+@app.route('/about', methods=['GET'])
 def about():
     return render_template('about.html')
 
-@app.route('/predict', methods=['GET','POST'])
+@app.route('/predict', methods=['GET', 'POST'])
 def predict():
     if request.method == 'POST':
         message = request.form['news']
         pred = fake_news_det(message)
         print(pred)
+        
         def predi(pred):
             if pred[0] == 1:
-              res="Prediction of the News :  Looking Fake News📰"
+                res = "Prediction of the News: Looking Fake News📰"
             else:
-              res="Prediction of the News : Looking Real News📰 "
+                res = "Prediction of the News: Looking Real News📰"
             return res
-        result=predi(pred)
-        return render_template("prediction.html",  prediction_text="{}".format(result))
+        
+        result = predi(pred)
+        return render_template("prediction.html", prediction_text="{}".format(result))
     else:
         return render_template('prediction.html', prediction="Something went wrong")
-
-
 
 if __name__ == '__main__':
     app.run(debug=True)
--- download_nltk_data.py
+++
@@ -1,9 +0,0 @@
-import nltk
-try:
-        # Download 'stopwords' and 'punkt' to a accessible location
-        nltk.download('stopwords', download_dir='/opt/render/project/src/.nltk_data', quiet=True)
-        nltk.download('punkt', download_dir='/opt/render/project/src/.nltk_data', quiet=True)
-        print("NLTK corpora downloaded successfully.")
-except Exception as e:
-        print(f"Error downloading NLTK corpora: {e}")
-    
